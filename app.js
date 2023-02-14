cff497e51e5afc9e16308fbcbab54cdcaf416e65
Alex Garrido Baldrich <alex.garrido.baldrich@gmail.com>
2023-02-14 10:26:19 +0100

    hbs set up

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
+//requiring .env file
 require('dotenv').config();
+
+//requiring express & app constant creation based on express framework
 const express = require('express');
 const app = express();
 
+//hbs config pointing to views folder
+app.set('view engine', 'hbs');
+app.set('views', `${__dirname}/views`);
 
 
-
-
-
-const port = process.env.PORT || "3002";
+// Conection with express server using port 3000.
+const port = process.env.PORT || "3000";
 app.listen(port, () => console.log(`Application running at port ${3000}`));
